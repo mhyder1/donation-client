e7fb8c6e5a7504e7ea1d4ee3d6594ea5fc07240e
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-10-13 17:04:31 -0500

    add routes to App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route, Switch} from 'react-router-dom'
+import Header from '../Header/Header'
+import PrivateRoute from '../../components/PrivateRoute/PrivateRoute'
+import PublicOnlyRoute from '../../components/PublicOnlyRoute/PublicOnlyRoute'
+import LandingPage from '../../routes/LandingPage/LandingPage'
+import LoginPage from '../../routes/LoginPage/LoginPage'
+import RegistrationPage from '../../routes/RegistrationPage/RegistrationPage'
+import DashboardPage from '../../routes/DashboardPage/DashboardPage'
+import NotFoundPage from '../../routes/NotFoundPage/NotFoundPage'
 
-function App() {
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  );
+class App extends Component {
+  state = { hasError: false }
+
+  static getDerivedStateFromError(error) {
+    console.error(error)
+    return { hasError: true }
+  }
+
+  render() {
+    const { hasError } = this.state
+    return (
+      <div className='App'>
+        <Header />
+        <main>
+          {hasError && (
+            <p>There was an error! Oh no!</p>
+          )}
+          <Switch>
+            <PrivateRoute
+              exact
+              path={'/'}
+              component={DashboardPage}
+            />
+            <PublicOnlyRoute
+              exact
+              path={'/'}
+              component={LandingPage}
+            />
+            <PublicOnlyRoute
+              path={'/register'}
+              component={RegistrationPage}
+            />
+            <PublicOnlyRoute
+              path={'/login'}
+              component={LoginPage}
+            />
+            <Route
+              component={NotFoundPage}
+            />
+          </Switch>
+        </main>
+      </div>
+    );
+  }
 }
+
 
 export default App;
