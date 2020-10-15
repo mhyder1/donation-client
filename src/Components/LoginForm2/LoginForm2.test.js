0bf28fae580b17f8cdb89eb84fb1246830e4a80d
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-10-15 13:20:55 -0500

    ensure tests work

--- src/Components/LoginForm2/LoginForm2.test.js
+++ src/Components/LoginForm2/LoginForm2.test.js
@@ -1,12 +1,12 @@
   
-import LoginForm from './LoginForm';
+import LoginForm2 from './LoginForm2.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-describe("LoginForm", () => {
+describe("LoginForm2", () => {
     it("form renders without crashing", () => {
       const div = document.createElement("div");
-      ReactDOM.render(<BrowserRouter><LoginForm /></BrowserRouter>, div);
+      ReactDOM.render(<BrowserRouter><LoginForm2 /></BrowserRouter>, div);
     });
   });
