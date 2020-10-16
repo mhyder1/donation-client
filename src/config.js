8f0f3a3a6a700a93840c8815c8bc5d41f668c134
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-10-16 16:21:18 -0500

    fix login

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
     API_ENDPOINT: 'https://cryptic-fjord-37790.herokuapp.com/api',
     //API_ENDPOINT: 'http://localhost:8000/api',
-    //TOKEN_KEY: '0e9fe658-17da-4642-ab59-025408e06105',
+    TOKEN_KEY: '0e9fe658-17da-4642-ab59-025408e06105',
   }
   
