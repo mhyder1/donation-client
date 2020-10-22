a3347f8d8b3a1f04242b718f8bc7e7ca772ed9e0
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-10-22 10:47:31 -0500

    remove whitespace and extra indents

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,60 +1,59 @@
 import React, { Component } from 'react'
 import './SearchForm.css';
 import { Input, Label } from '../Form/Form'
-import Button from '../Button/Button'
 
 class SearchForm extends Component {
-    state = { error: null }
+  state = { error: null }
 
-    searchBoxRef = React.createRef();
+  searchBoxRef = React.createRef();
 
-    componentDidMount() {
-        this.postRender();
-        this.searchBoxRef.current.focus();
-    }
-    
-    componentDidUpdate() {
-        this.postRender();
-    }
+  componentDidMount() {
+      this.postRender();
+      this.searchBoxRef.current.focus();
+  }
+  
+  componentDidUpdate() {
+    this.postRender();
+  }
 
-    render() {
-        const { error } = this.state;
-        return (
-            <form
-              className="search-form form"
-              onSubmit={this.handleSubmit}
-            >
-            <div role='alert'>
-              {error && <p>{error}</p>}
-            </div>
-            <div className="form-line">
-              <Label htmlFor='search-form-input'>
-                Look for sites to donate:
-              </Label>
-              <Input
-                ref={this.searchBoxRef}
-                id='search-form-input'
-                name='search'
-                placeholder='zipcode/address'
-              />
-            </div>
-          </form>
-        );
-    }
+  render() {
+    const { error } = this.state;
+    return (
+      <form
+        className="search-form form"
+        onSubmit={this.handleSubmit}
+      >
+        <div role='alert'>
+          {error && <p>{error}</p>}
+        </div>
+        <div className="form-line">
+          <Label htmlFor='search-form-input'>
+            Look for sites to donate:
+          </Label>
+          <Input
+            ref={this.searchBoxRef}
+            id='search-form-input'
+            name='search'
+            placeholder='zipcode/address'
+          />
+        </div>
+      </form>
+    );
+  }
 
-    postRender() {
-      const autocomplete = new window.google.maps.places.Autocomplete(this.searchBoxRef.current);
-      autocomplete.addListener("place_changed", () => {
-        const { map } = window;
-        if(!map) // prevent this component from blowing up if tested without an attached map
-          return;
-        // height of the top area, plus 4rem for App (margin-top + padding-top + padding-bottom + margin-bottom)
-        const offset = this.searchBoxRef.current.getBoundingClientRect().bottom
-          + 4 * parseFloat(window.getComputedStyle(document.documentElement).fontSize);
-        console.log(`map-padding-top: ${offset}`);
-        map.fitBounds(autocomplete.getPlace().geometry.viewport, { top: offset });
-      });
-    }
+  postRender() {
+    const autocomplete = new window.google.maps.places.Autocomplete(this.searchBoxRef.current);
+    autocomplete.addListener("place_changed", () => {
+      const { map } = window;
+      if(!map) // prevent this component from blowing up if tested without an attached map
+        return;
+      // height of the top area, plus 4rem for App (margin-top + padding-top + padding-bottom + margin-bottom)
+      const offset = this.searchBoxRef.current.getBoundingClientRect().bottom
+        + 4 * parseFloat(window.getComputedStyle(document.documentElement).fontSize);
+      console.log(`map-padding-top: ${offset}`);
+      map.fitBounds(autocomplete.getPlace().geometry.viewport, { top: offset });
+    });
+  }
 }
 
 export default SearchForm;
